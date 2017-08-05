606cb2b8b3b80c311a67ea3260be2746b9b223e3
ThirupathiReddy <tvajjala@gmail.com>
2017-08-04 20:46:50 -0500

    initial draft

--- src/main/java/com/cisco/main/InvoiceReport.java
+++ src/main/java/com/cisco/main/InvoiceReport.java
@@ -1,37 +1,31 @@
 package com.cisco.main;
 
-import com.sun.xml.internal.bind.v2.util.ByteArrayOutputStreamEx;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.util.Collections;
-import javax.swing.table.DefaultTableModel;
-import net.sf.jasperreports.engine.JRExporterParameter;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.data.JRTableModelDataSource;
 import net.sf.jasperreports.engine.export.JRPdfExporter;
 import net.sf.jasperreports.view.JasperViewer;
 import org.springframework.core.io.ClassPathResource;
+
+import javax.swing.table.DefaultTableModel;
 public class InvoiceReport {
-    
-    public static void main(String[] args) throws Exception{
-        JRPdfExporter exporter=new JRPdfExporter();
-        ClassPathResource resource=new ClassPathResource("Invoice.jrxml");
-      
-        JasperReport jasperReport= JasperCompileManager.compileReport( resource.getInputStream());
-        JasperPrint jasperPrint= JasperFillManager.fillReport(jasperReport, null,new JRTableModelDataSource(tableModelData()));
-        JasperViewer jasperViewer = new JasperViewer(jasperPrint);
+
+    public static void main(final String[] args) throws Exception {
+        final JRPdfExporter exporter = new JRPdfExporter();
+        final ClassPathResource resource = new ClassPathResource("Invoice.jrxml");
+
+        final JasperReport jasperReport = JasperCompileManager.compileReport(resource.getInputStream());
+        final JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null, new JRTableModelDataSource(tableModelData()));
+        final JasperViewer jasperViewer = new JasperViewer(jasperPrint);
         jasperViewer.setVisible(true);
      
     }
     
     private static DefaultTableModel tableModelData() {
-        String[] columnNames = {"Id", "Name", "Department", "Email"};
-        String[][] data = {
+        final String[] columnNames = {"Id", "Name", "Department", "Email"};
+        final String[][] data = {
             {"111", "G Conger", " Orthopaedic", "jim@wheremail.com"},
             {"222", "A Date", "ENT", "adate@somemail.com"},
             {"333", "R Linz", "Paedriatics", "rlinz@heremail.com"},
@@ -43,6 +37,7 @@ public class InvoiceReport {
             {"999", "P Patel", "Nephrology", "ppatel@gomail.com"},
             {"101", "C Comer", "Nephrology", "ccomer@whymail.com"}
         };
-      return new DefaultTableModel(data, columnNames);
+        
+        return new DefaultTableModel(data, columnNames);
     }
 }
